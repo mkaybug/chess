1ee3776fa232b069d82ec3da60e98462f3ce0c64
Makayla Black <makayla.negro@gmail.com>
2024-02-29 21:52:38 -0700

    Passed GameServiceTests and added Request records for CreateGame and JoinGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,13 +1,18 @@
 package service;
 
+import chess.ChessGame;
+
 import dataAccess.DataAccessException;
 import dataAccess.GameDAO;
 import dataAccess.AuthDAO;
 import dataAccess.UserDAO;
 import model.AuthData;
+import model.CreateGameRequest;
 import model.GameData;
+import model.JoinGameRequest;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public class GameService {
   private final AuthDAO authDataAccess;
@@ -18,14 +23,10 @@ public class GameService {
     this.gameDataAccess = gameDataAccess;
   }
 
-  public AuthData authenticateUser(AuthData auth) throws DataAccessException {
-    // Check if auth already exists
-    AuthData existingAuth = getAuth(auth.authToken());
+  public void authenticateUser(String authToken) throws DataAccessException {
+    AuthData existingAuth = getAuth(authToken);
     if (existingAuth == null) {
       throw new DataAccessException("Error: unauthorized");
-    }
-    else {
-      return existingAuth;
     }
   }
 
@@ -33,15 +34,66 @@ public class GameService {
     return authDataAccess.getAuth(authToken);
   }
 
-  public Collection<GameData> listGames(AuthData auth) throws DataAccessException {
+  public Collection<GameData> listGames(String authToken) throws DataAccessException {
     try {
-      authenticateUser(auth);
+      authenticateUser(authToken);
       // Return list of games
       return gameDataAccess.listGames();
     }
     catch (DataAccessException e) {
       throw new DataAccessException("Error: unauthorized");
     }
+  }
+
+  public GameData createGame(String authToken, CreateGameRequest request) throws DataAccessException {
+    try {
+      authenticateUser(authToken);
+    }
+    catch (DataAccessException e) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+
+    if (request.gameName() == null) {
+      throw new DataAccessException("Error: bad request");
+    }
+
+    int idNum = 1;
+    while (getGame(idNum) != null) {
+      idNum += 1;
+    }
+    return addGame(new GameData(idNum, null, null, request.gameName(), new ChessGame()));
+  }
+
+  public void joinGame(String authToken, JoinGameRequest request) throws DataAccessException {
+    // Authenticate user
+    try {
+      authenticateUser(authToken);
+    }
+    catch (DataAccessException e) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+
+    // If gameID is null or doesn't correspond to a real game
+    if (request.gameID() == 0 || getGame(request.gameID()) == null) {
+      throw new DataAccessException("Error: bad request");
+    }
+
+    // Get the game
+    GameData game = getGame(request.gameID());
+    if (Objects.equals(request.playerColor(), "WHITE")) {
+      if (game.whiteUsername() != null) {
+        throw new DataAccessException("Error: already taken");
+      }
+      putGame(new GameData (game.gameID(), getAuth(authToken).username(), null, game.gameName(), game.game()));
+    }
+    if (Objects.equals(request.playerColor(), "BLACK")) {
+      if (game.blackUsername() != null) {
+        throw new DataAccessException("Error: already taken");
+      }
+      putGame(new GameData (game.gameID(), null, getAuth(authToken).username(), game.gameName(), game.game()));
+    }
+    // Observer -> empty string
+    // Make sure the game exists (since we've already done that, we do nothing
   }
 
   public GameData addGame(GameData game) throws DataAccessException {
--- server/src/test/java/serviceTests/GameServiceTests.java
+++ server/src/test/java/serviceTests/GameServiceTests.java
@@ -7,8 +7,7 @@ import dataAccess.MemoryGameDAO;
 import dataAccess.MemoryUserDAO;
 import dataAccess.DataAccessException;
 
-import model.AuthData;
-import model.GameData;
+import model.*;
 
 import service.ClearService;
 import service.GameService;
@@ -45,19 +44,95 @@ public class GameServiceTests {
     userService.addAuth(new AuthData("token3", "black2"));
 
     // Negative test -> not authorized so listGames fails
-    assertThrows(DataAccessException.class, () -> gameService.listGames(testAuth));
+    assertThrows(DataAccessException.class, () -> gameService.listGames(testAuth.authToken()));
 
     // Positive test -> listGames is empty
     userService.addAuth(testAuth);
-    assertIterableEquals(new ArrayList<>(), gameService.listGames(testAuth));
+    assertIterableEquals(new ArrayList<>(), gameService.listGames(testAuth.authToken()));
 
     // Positive test -> listGames is not empty
     expected.add(gameService.addGame(new GameData(1, "white1", "black1", "game1", new ChessGame())));
     expected.add(gameService.addGame(new GameData(2, "white2", "black2", "game2", new ChessGame())));
     expected.add(gameService.addGame(new GameData(3, "white3", "black3", "game3", new ChessGame())));
 
-    var actual = gameService.listGames(testAuth);
+    var actual = gameService.listGames(testAuth.authToken());
     assertIterableEquals(expected, actual);
+  }
+
+  @Test
+  void createGame() throws DataAccessException {
+    GameData game = new GameData(1, null, null, "game1", new ChessGame());
+    AuthData auth = new AuthData("auth", "username");
+    UserData user = new UserData("username", "password", "email@gmail.com");
+    CreateGameRequest request = new CreateGameRequest(game.gameName());
+    CreateGameRequest nullRequest = new CreateGameRequest(null);
+
+    // Negative test case -> authentication fails
+    assertThrows(DataAccessException.class, () -> gameService.createGame(auth.authToken(), request));
+
+    userService.addUser(user);
+    userService.addAuth(auth);
+
+    // Positive test case (testing each field separately because new ChessGames will always have different addresses
+    GameData newGame = gameService.createGame(auth.authToken(), request);
+    assertEquals(game.whiteUsername(), newGame.whiteUsername());
+    assertEquals(game.blackUsername(), newGame.blackUsername());
+    assertEquals(game.gameName(), newGame.gameName());
+
+    // Adding a second game
+    GameData secondGame = gameService.createGame(auth.authToken(), request);
+    assertEquals(game.whiteUsername(), newGame.whiteUsername());
+    assertEquals(game.blackUsername(), newGame.blackUsername());
+    assertEquals(game.gameName(), newGame.gameName());
+
+    // Negative test case -> null gameName so fails
+    assertThrows(DataAccessException.class, () -> gameService.createGame(auth.authToken(), nullRequest));
+  }
+
+  @Test
+  void joinGame() throws DataAccessException {
+    GameData game = new GameData(1, null, null, "game1", new ChessGame());
+
+    AuthData auth1 = new AuthData("auth1", "username1");
+    AuthData auth2 = new AuthData("auth2", "username2");
+
+    UserData user1 = new UserData("username1", "password1", "email1@gmail.com");
+    UserData user2 = new UserData("username2", "password2", "email2@gmail.com");
+
+    JoinGameRequest requestWhite = new JoinGameRequest("WHITE", 1);
+    JoinGameRequest requestBlack = new JoinGameRequest("BLACK", 1);
+    JoinGameRequest nullRequest = new JoinGameRequest("WHITE", 0);
+
+    // Negative test case -> authentication fails
+    assertThrows(DataAccessException.class, () -> gameService.joinGame(auth1.authToken(), requestWhite));
+
+    userService.addUser(user1);
+    userService.addUser(user2);
+    userService.addAuth(auth1);
+    userService.addAuth(auth2);
+
+    // Negative test case -> game doesn't exist
+    assertThrows(DataAccessException.class, () -> gameService.joinGame(auth1.authToken(), requestWhite));
+
+    gameService.addGame(game);
+
+    // Negative test case -> invalid gameID
+    assertThrows(DataAccessException.class, () -> gameService.joinGame(auth1.authToken(), nullRequest));
+
+    // Positive test case -> white passes
+    gameService.joinGame(auth1.authToken(), requestWhite);
+
+    assertEquals("username1", gameService.getGame(game.gameID()).whiteUsername());
+
+    // Negative test case -> white already taken
+    assertThrows(DataAccessException.class, () -> gameService.joinGame(auth1.authToken(), requestWhite));
+
+    // Positive test case -> black passes
+    gameService.joinGame(auth2.authToken(), requestBlack);
+    assertEquals("username2", gameService.getGame(game.gameID()).blackUsername());
+
+    // Negative test case -> black already taken
+    assertThrows(DataAccessException.class, () -> gameService.joinGame(auth2.authToken(), requestBlack));
   }
 
   @Test
@@ -71,5 +146,4 @@ public class GameServiceTests {
     var actual = clearService.listGames();
     assertIterableEquals(expected, actual);
   }
-
 }
